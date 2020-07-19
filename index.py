c44de2f312c2a1bfe3a1d28d67e5d31157eae0a5
ata555 <agata.kwiek@nextapps.pl>
2020-07-19 12:54:32 +0200

    Added extracting all premise-conclusion pairs

--- index.py
+++ index.py
@@ -4,26 +4,20 @@ import string
 from pprint import pprint
 from nltk.tokenize import word_tokenize
 import nltk
+import glob
+from nltk.corpus import wordnet as wn
+import spacy
+
 # from spacy.lang.pl import STOP_WORDS
 # sources: https://github.com/bieli/stopwords/blob/master/polish.stopwords.txt and https://github.com/stopwords-iso/stopwords-pl
 
 
-with open('debateTVP/nodeset16955.json') as f:
-    data = json.loads(f.read())
+maps = glob.glob("maps/*.json")
 
-#Creating nodesById dictionary which has nodeID as key and whole node as value for more efficient data extraction.
-nodesById = {}
-for i, node in enumerate(data['nodes']):
-  nodesById[node['nodeID']] = node
-
-#Premises are nodes that have ingoing edges that are type 'RA' and outgoing edges that are type 'I'.
-premises = [x for x in data['edges'] if nodesById[x['fromID']]['type'] == 'I' and nodesById[x['toID']]['type'] == 'RA' ]
-
-#Conclusions are nodes that have ingoing edges that are type 'I' and outgoing edges that are type 'RA'.
-conclusions = [x for x in data['edges'] if nodesById[x['toID']]['type'] == 'I' and nodesById[x['fromID']]['type'] == 'RA' ]
-
-# conclusionPremiseDict Create dictionary of pairs with an identifier with the following form: 
-# {id: {"conclusion": <SINGLE_CONCLUSION>, "premises":[<LIST_OF_PREMISES>]}}
+'''
+    conclusionPremiseDict Create dictionary of pairs with an identifier with the following form: 
+    {id: {"conclusion": <SINGLE_CONCLUSION>, "premises":[<LIST_OF_PREMISES>]}}
+'''
 def conclusionPremiseDict(premises, conclusions):
     pairs = {}
     for i, x in enumerate(conclusions):
@@ -36,19 +30,79 @@ def conclusionPremiseDict(premises, conclusions):
                 
     return pairs
 
-edgePairs = conclusionPremiseDict(premises, conclusions)
-    
-# aduPairs create list of ADU pairs containing connected conclusion and premise [[conclusion, premise]]
-def aduPairs(edgePairs):
+'''
+    aduPairs create list of ADU pairs containing connected conclusion and premise [[conclusion, premise]]
+'''
+def aduPairs(edgePairs, nodesById):
     aduPair = []
     for pair in edgePairs.values():
         for p in pair['premises']:
           aduPair.append([nodesById[pair['conclusion']['toID']]['text'], nodesById[p['fromID']]['text']])
     return(aduPair)
+'''
+    pairs creates conclusion - premise pairs for one map
+'''
+def pairs(map):
+    with open(map) as f:
+        data = json.loads(f.read())
+    #Creating nodesById dictionary which has nodeID as key and whole node as value for more efficient data extraction.
+    nodesById = {}
+    for i, node in enumerate(data['nodes']):
+        nodesById[node['nodeID']] = node
 
-# adus = aduPairs(edgePairs)
+    #Premises are nodes that have ingoing edges that are type 'RA' and outgoing edges that are type 'I'.
+    premises = [x for x in data['edges'] if nodesById[x['fromID']]['type'] == 'I' and nodesById[x['toID']]['type'] == 'RA' ]
 
+    #Conclusions are nodes that have ingoing edges that are type 'I' and outgoing edges that are type 'RA'.
+    conclusions = [x for x in data['edges'] if nodesById[x['toID']]['type'] == 'I' and nodesById[x['fromID']]['type'] == 'RA' ]
+    edgePairs = conclusionPremiseDict(premises, conclusions)
+    adus = aduPairs(edgePairs, nodesById)
+    return adus
 
+'''
+    allPairs is list of all premise - conclusion pairs
+'''
+allPairs = []
+for m in maps:
+    p = pairs(m)
+    allPairs.extend(p)
+
+'''
+     http://zil.ipipan.waw.pl/SpacyPL
+'''
+nlp = spacy.load('pl_spacy_model')
+
+'''
+    semSimilarity takes two sentences, tokenize them, lemmatize words, then for every word in first sentence finds most 
+    similar word in second sentence; return list of max similarity for words
+'''
+def sentenceSimilarity (s1, s2):
+    lem1 = nlp(s1)
+    lem2 = nlp(s2)
+    maxSimilarity = []
+    for token1 in lem1:
+        if wn.synsets(token1.lemma_) != []:
+            wordSimilarity = []
+            word1 = wn.synsets(token1.lemma_)[0]
+            for token2 in lem2:
+                if wn.synsets(token2.lemma_) != []:
+                    word2 = wn.synsets(token2.lemma_)[0]
+                    wordSimilarity.append(word1.wup_similarity(word2))
+            wordSimilarityWNone = [x for x in wordSimilarity if x]
+            if len(wordSimilarityWNone) != 0:
+                maxSimilarity.append(max(wordSimilarityWNone))       
+    return maxSimilarity
+    
+'''
+    similarity takes two sentenes, returns sum of average value of two lists and divide this by two
+'''
+def similarity(s1, s2):
+    max1 = sentenceSimilarity(s1, s2)
+    max2 = sentenceSimilarity(s2, s1)
+
+    return (sum(max1) / float(len(max1)) + sum(max2) / float(len(max2))) / 2
+
+print(similarity(allPairs[0][0], allPairs[0][1]))
 
 # removeStopwords remove words that are the most common words in any natural language, this function also removes punctuation
 # def removeStopwords(adus):
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from nltk import word_tokenize, pos_tag
+
 from nltk.corpus import wordnet as wn
 import spacy
 
